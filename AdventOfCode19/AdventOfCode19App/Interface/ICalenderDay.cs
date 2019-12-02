ed9b8c24280c7c87a87a8fd26c81dbfa0284d04b
Anton Patron <patronant@gmail.com>
2019-12-03 00:34:54 +0100

    Day 2 check in.

---
+++ AdventOfCode19/AdventOfCode19App/Interface/ICalenderDay.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace AdventOfCode19App.Interface
+{
+    public interface ICalenderDay
+    {
+        string Header();
+
+        Task<string> Run();
+    }
+}
